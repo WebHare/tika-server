8c792890b3e4f552a13c25f155d7ce3a156e36c7
Arnold Hendriks <arnold@webhare.nl>
2021-01-09 15:50:28 +0100

    increase timeout

--- dropins/opt/container/launch.sh
+++ dropins/opt/container/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec java -jar /tika-server-${TIKA_VERSION}.jar -config /opt/container/data/tika-config.xml -spawnChild -maxFiles 1000 -h 0.0.0.0 "$@"
+exec java -jar /tika-server-${TIKA_VERSION}.jar -config /opt/container/data/tika-config.xml -spawnChild -maxFiles 1000 -h 0.0.0.0 --taskTimeoutMillis 600000 "$@"
